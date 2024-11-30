6ad2df3f2703c0e92ef1ac2f15ac60f1a3d3dfd8
Nick Torosian <goodvibrations32@protonmail.com>
2024-11-30 15:00:48 +0200

    fix(:editor): persist text scale after mode guess When a buffer is in fundamental mode saving the buffer should not rescale the text to default.
    Fix: #7378

--- lisp/doom-editor.el
+++ lisp/doom-editor.el
@@ -136,6 +136,13 @@ or file path may exist now."
              (set-auto-mode)
              (not (eq major-mode 'fundamental-mode)))))))
 
+(defadvice! doom--preserve-text-scale-for-guess-mode-a (fn)
+  "Preserve the text scale for `set-auto-mode'."
+  :around #'set-auto-mode
+  (let ((scale text-scale-mode-amount))
+    (funcall fn)
+    (text-scale-set scale)))
+
 (defadvice! doom--shut-up-autosave-a (fn &rest args)
   "If a file has autosaved data, `after-find-file' will pause for 1 second to
 tell you about it. Very annoying. This prevents that."
